a54226ccdfc66bac441a7469a8591486aad2b719
diruocco-aniellomarco <aniello.marco.dr@gmail.com>
2023-08-26 13:53:11 +0200

    milestone da 0 a 2

--- js/main.js
+++ js/main.js
@@ -26,56 +26,71 @@ const images = [
     },
   ];
 
-// console.log(images[0].image);
-  
-  const next = document.getElementById("next");
-  const prev = document.getElementById("prev");
-  const positionSlides = document.getElementById("position_slides");
 
+// mi prendo le posizioni delle frecce e del contenitore delle slides
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+const positionSlides = document.getElementById("position_slides");
 
+// contatore per mettere la classe actine (display block) e farla vedere
 let visibile;
 
 for (let i = 0; i < images.length; i++ ) {
-  console.log(images[i].image);
+
+  //quando creo le slides, se è la prima rendila visibile
   if (i == 0) {
     visibile = 'active';
   } else {
     visibile = '';
   }
+
+  //creo le slides
   positionSlides.innerHTML += `
       <div class="slide ${visibile}">
         <img src="./${images[i].image}" alt="">
+      </div>
+      <div class="testi text-end me-3 ${visibile}">
+        <h1>${images[i].title}</h1>
+        <p>${images[i].text}</p>
       </div>`
 
-
 }
+//prendo tutte le slides e tutti i testi 
+const slides = document.getElementsByClassName("slide");
+const testi = document.getElementsByClassName("testi");
 
-  const slides = document.getElementsByClassName("slide");
+// indice
+let index = 0;
+
+next.addEventListener('click', function() {
   
-  let index = 0;
+  //prima rimuovo la classe active dalle immagini e dai testi
+  slides[index].classList.remove('active');
+  testi[index].classList.remove('active');
+  //incremento l'indice se è minore della lunghezza di tutti gli elementi (essendo "indice" metto meno 1 o se no sforerebbe) 
+  //oppure se se arrivo alla fine delle slide lo rimetto a 0
+  if (index < (slides.length -1)) {
+    index++;
+  } else {
+    index = 0;
+  }
+  // con il nuovo indice aggiungo la classe active per renderli visibili (display block)
+  slides[index].classList.add('active');
+  testi[index].classList.add('active');
+  
+})
 
-  next.addEventListener('click', function() {
-    
-    // document.querySelector('.slide.active').classList.remove('active');
-    slides[index].classList.remove('active');
-    if (index < (slides.length -1)) {
-      index++;
-    } else {
-      index = 0;
-    }
-    slides[index].classList.add('active');
-    console.log(index);
-  })
-  console.log(slides.length);
-  prev.addEventListener('click', function() {
-    // document.querySelector('.slide.active').classList.remove('active');
-    slides[index].classList.remove('active');
-    // index--;
-    if (index == 0) {
-      index = (slides.length - 1);
-    } else {
-      index--;
-    }
-    slides[index].classList.add('active');
-    console.log(index);
-  })
+//come next ma al contrario
+prev.addEventListener('click', function() {
+  
+  slides[index].classList.remove('active');
+  testi[index].classList.remove('active');
+  if (index == 0) {
+    index = (slides.length - 1);
+  } else {
+    index--;
+  }
+  slides[index].classList.add('active');
+  testi[index].classList.add('active');
+  
+})
